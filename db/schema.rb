b84610870b25bca9fa8cb6c7f9bff92978340805
Daniel Kay <Daniel_Kay@Baylor.edu>
2014-10-25 02:55:40 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,4 @@ ActiveRecord::Schema.define(version: 20141025041315) do
     t.string   "location"
   end
 
-  create_table "scrapers", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
